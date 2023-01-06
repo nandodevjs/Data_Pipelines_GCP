892c662e878b5fff22249514d22f14c6c61057b1
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2023-01-05 22:30:36 -0300

    creating simple bucket

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "google_storage_bucket" "gcs_bucket" {
+    name = "teste-bucket-radom-5645645645"
+    location = var.region
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "value"
+region = "us-central1"
